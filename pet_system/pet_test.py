fdd9a5f611b9dea30f31fda9a73cc25c5a79b166
emmanpestan <emmanuel.empestan@gmail.com>
2026-06-05 06:05:37 +0800

    Added Prompt and pet attributes via setter methods

--- pet_system/pet_test.py
+++ pet_system/pet_test.py
@@ -2,3 +2,11 @@ from Pet import Pet
 
 def main():
     my_pet = Pet()
+
+    name        = input("Enter your pet's name: ")
+    animal_type = input("Enter the type of animal: ")
+    age         = int(input("Enter your pet's age: "))
+
+    my_pet.set_name(name)
+    my_pet.set_animal_type(animal_type)
+    my_pet.set_age(age)
